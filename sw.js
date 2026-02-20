70180ba17ac19dd53104e26719adc0abdf65fca1
pablopaiporta-eng <pablopaiporta@gmail.com>
2026-02-20 11:05:32 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+const CACHE_NAME = "trader-pro-v1";
+const urlsToCache = [
+  "/Calculadora-trader/",
+  "/Calculadora-trader/index.html",
+  "/Calculadora-trader/manifest.json",
+  "/Calculadora-trader/icon-192.png",
+  "/Calculadora-trader/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
